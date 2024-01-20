dcc44b218855d5fdae55c129e046f6187489d315
Carlos Zambrano <>
2024-01-19 22:56:44 -0500

    ADD: Metadata in product page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,9 +1,32 @@
 export const revalidate = 10; // 7 dias aprox
 
+import { Metadata, ResolvingMetadata } from 'next';
+import { notFound } from 'next/navigation';
 import { getProductBySlug } from '@/actions';
 import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector, StockLabel } from '@/components';
 import { titleFont } from '@/config/fonts';
-import { notFound } from 'next/navigation';
+
+
+export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+    // read route params
+    const slug = params.slug
+
+    // fetch data
+    const product = await getProductBySlug(slug)
+
+    // optionally access and extend (rather than replace) parent metadata
+    // const previousImages = (await parent).openGraph?.images || []
+
+    return {
+        title: product?.title || 'Producto no encontrado',
+        description: product?.description || '',
+        openGraph: {
+            title: product?.title || 'Producto no encontrado',
+            description: product?.description || '',
+            images: [`/products/${product?.images[1]}`],
+        },
+    }
+}
 
 interface Props {
     params: { slug: string };
