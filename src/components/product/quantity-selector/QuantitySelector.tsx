681de480f80c29e5eb7a6268a57f2a0e4d0a58e2
Carlos Zambrano <>
2024-01-20 01:25:43 -0500

    ADD Store Global to products in cart

--- src/components/product/quantity-selector/QuantitySelector.tsx
+++ src/components/product/quantity-selector/QuantitySelector.tsx
@@ -1,30 +1,29 @@
 'use client';
 
-import { useState } from 'react';
 import { IoAddCircleOutline, IoRemoveCircleOutline } from 'react-icons/io5';
 
 interface Props {
     quantity: number;
+    onQuantityChange: (quantity: number) => void
 }
 
-export const QuantitySelector = ({ quantity }: Props) => {
-    const [count, setCount] = useState(quantity);
+export const QuantitySelector = ({ quantity, onQuantityChange }: Props) => {
 
-    const onQuantityChange = (value: number) => {
-        if (count + value < 1) return;
+    const onValueChange = (value: number) => {
+        if (quantity + value < 1) return;
 
-        setCount(state => state + value);
+        onQuantityChange(quantity + value);
     };
 
     return (
         <div className='flex'>
-            <button onClick={() => onQuantityChange(-1)}>
+            <button onClick={() => onValueChange(-1)}>
                 <IoRemoveCircleOutline size={30} />
             </button>
 
-            <span className='w-20 mx-3 px-5 bg-gray-200 text-center rounded'>{count}</span>
+            <span className='w-20 mx-3 px-5 bg-gray-200 text-center rounded'>{quantity}</span>
 
-            <button onClick={() => onQuantityChange(1)}>
+            <button onClick={() => onValueChange(1)}>
                 <IoAddCircleOutline size={30} />
             </button>
         </div>
